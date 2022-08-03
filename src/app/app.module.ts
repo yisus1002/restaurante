9b8c0b08ef60dce85c614a3c1d899dc1468f9b21
yisus1002 <yisusgarcia1002@gmail.com>
2022-08-02 21:41:02 -0500

    Se creo el navbar, el home, el plato y sus opciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoguinComponent } from './components/loguin/loguin.component';
+
+import {  RouterModule } from '@angular/router';
+//Haccer peticiones 
+import { HttpClientModule } from '@angular/common/http';
+
+
+//Importar rutas
+import { ROUTES } from './app.routes';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PlatoComponent } from './components/plato/plato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoguinComponent,
+    NavbarComponent,
+    PlatoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(ROUTES , {useHash: true}),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router'; 
+import { HomeComponent } from './components/home/home.component';
+
+
+export const ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    // { path: 'search', component: SearchComponent }, 
+    // { path: 'artist/:id', component: ArtistaComponent }, 
+    { path: '', pathMatch :'full', redirectTo: 'home'}, 
+    { path: '**', pathMatch :'full', redirectTo: 'home'}, 
+];
