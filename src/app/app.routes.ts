2a54e5e43f85aa3a2f5454a39e5c6798978433de
yisus1002 <yisusgarcia1002@gmail.com>
2022-08-06 15:01:59 -0500

    Se agrego la validacion a la ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router'; 
 import { HomeComponent } from './components/home/home.component';
 import { LoguinComponent } from './components/loguin/loguin.component';
+import { 
+    AuthGuardService as AuthGuard
+} from './services/auth-guard.service';
 
 
 export const ROUTES: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'loguin', component: LoguinComponent },
+    { path: 'home', component: HomeComponent , canActivate: [AuthGuard]},
+    { path: 'login', component: LoguinComponent },
     // { path: 'search', component: SearchComponent }, 
     // { path: 'artist/:id', component: ArtistaComponent }, 
     { path: '', pathMatch :'full', redirectTo: 'home'}, 
