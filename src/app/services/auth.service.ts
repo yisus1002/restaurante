c45fba5dc65f61b2ae7046ccd8f2cffa24a46a9b
yisus1002 <yisusgarcia1002@gmail.com>
2022-08-06 17:46:44 -0500

    Se agrego una alerta indicando que el login fue exitoso, redirigiendo al home

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,8 @@ import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
-import { map, Observable } from 'rxjs';
+import { map, Observable, timeInterval } from 'rxjs';
+import { TimeInterval } from 'rxjs/internal/operators/timeInterval';
 import Swal from 'sweetalert2';
 
 @Injectable({
@@ -45,8 +46,21 @@ export class AuthService {
         next: (response)=>{
           console.log(response)
           this.estado=false
-          localStorage.setItem('token', JSON.stringify(response.token))
-          
+          localStorage.setItem('token', JSON.stringify(response.token));
+          Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Successful login!',
+            showConfirmButton: false,
+            timer: 1500,
+            timerProgressBar: true,
+            didOpen:()=>{
+              Swal.showLoading();
+            }
+          })
+          setTimeout(()=>{
+            this.router.navigate(['home'])
+          },1500)
         },
         error: (error)=>{
           this.estado=false
