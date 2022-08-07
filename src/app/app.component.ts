c6913787ce587013fdb329d2df5f8ff64bd13805
yisus1002 <yisusgarcia1002@gmail.com>
2022-08-06 19:01:51 -0500

    Se valido que el navbar no se muestre en el login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { NavigationEnd, Router, Event } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Restaurante';
+export class AppComponent implements OnInit {
+  // title = 'Restaurante';
+  currentRoute?:string;
+  constructor(public router: Router){
+    this.verificarRuta();
+
+  }
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+  }
+  verificarRuta(){
+    this.router.events.subscribe((event: Event) => {
+            
+      if (event instanceof NavigationEnd) {
+          this.currentRoute = event.url;
+            // console.log(event);
+
+      }
+      // console.log(this.currentRoute)
+  });
+  }
+
 }
