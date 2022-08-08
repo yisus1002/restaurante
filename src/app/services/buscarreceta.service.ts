42ad361e14469c8025e8699e6a1ea6f8504783ca
yisus1002 <yisusgarcia1002@gmail.com>
2022-08-08 16:15:26 -0500

    Se agregaron validaciones si la data no arroja nada

--- src/app/services/buscarreceta.service.ts
+++ src/app/services/buscarreceta.service.ts
@@ -9,13 +9,14 @@ export class BuscarrecetaService {
   receta:any[]=[];
   menu:any[]=[1,2,3,4];
   loading:boolean=false;
+  resultado:boolean=false
 
   constructor(public http: HttpClient) {
     // this.obtener()
    }
 
   getRecetas(termino:string, ){
-    const url =`https://api.spoonacular.com/recipes/complexSearch?query=${termino}&apiKey=8a0b99c7da0c4d1dadfa51797ad3b73e`;
+    const url =`https://api.spoonacular.com/recipes/complexSearch?query=${termino}&apiKey=2ffb93eb9d1f4806ab91ae151b9a644d`;
     
     return this.http.get(url).pipe(map((data:any)=> data))
   }
@@ -27,16 +28,21 @@ export class BuscarrecetaService {
     }))
     .subscribe({
       next: (data:any)=>{
+        // this.resultado=true;
         if(data.results.length>=2){
         this.receta=data.results;
         this.loading=false;
+        this.resultado=false;
         console.log(data.results)
         console.log(this.receta)
+        }else if(data.results.length<=2){
+          this.resultado=true;
         }
       },
       error:(err)=> {
         this.loading=false;
-        console.warn(err)
+        this.resultado=false;
+        console.warn(err.error.message)
       },
     })
   }
