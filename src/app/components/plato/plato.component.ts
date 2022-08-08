56fb556811db16d031d453525f24e4b3377f802e
yisus1002 <yisusgarcia1002@gmail.com>
2022-08-07 21:47:28 -0500

    Se corrigio el componente plato y se le creo un input para recibir datos de la api

--- src/app/components/plato/plato.component.ts
+++ src/app/components/plato/plato.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-plato',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlatoComponent implements OnInit {
 
+  @Input() items: any[]=[];
+  
   constructor() { }
 
   ngOnInit(): void {
