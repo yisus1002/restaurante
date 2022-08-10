99e687342becc8573c8c7aba0e544c276506c902
yisus1002 <yisusgarcia1002@gmail.com>
2022-08-09 19:55:23 -0500

    Se mejoró el metodo buscar permitiendo que se obtenga la informacion de cada receta

--- src/app/services/buscarreceta.service.ts
+++ src/app/services/buscarreceta.service.ts
@@ -9,32 +9,48 @@ export class BuscarrecetaService {
   receta:any[]=[];
   menu:any[]=[1,2,3,4];
   loading:boolean=false;
-  resultado:boolean=false
+  resultado:boolean=false;
+  id:any[]=[];
+  api:string='https://api.spoonacular.com/recipes/';
+  apykey:string='2ffb93eb9d1f4806ab91ae151b9a644d';
 
   constructor(public http: HttpClient) {
     // this.obtener()
    }
 
   getRecetas(termino:string, ){
-    const url =`https://api.spoonacular.com/recipes/complexSearch?query=${termino}&apiKey=2ffb93eb9d1f4806ab91ae151b9a644d`;
+    const url =`${this.api}complexSearch?query=${termino}&apiKey=${this.apykey}`;
     
+    return this.http.get(url).pipe(map((data:any)=> data))
+  };
+  getInformationReceta(ids:any){
+    const url = `${this.api}informationBulk?ids=${ids}&apiKey=${this.apykey}`
     return this.http.get(url).pipe(map((data:any)=> data))
   }
   obtener(termino:string){
     this.loading=true;
     this.getRecetas(termino)
     .pipe(finalize(()=>{
-      this.loading=false;
+      this.getInformationReceta(this.id.toString())
+      .pipe(finalize(()=>{
+        this.loading=false;
+      }))
+      .subscribe({
+        next: (data:any)=> {
+          this.receta=data;
+          console.log(data);
+          this.loading=false;
+          this.resultado=false;
+        },
+        error: (err)=>{
+          console.warn(err.error.message)
+        }
+      })
     }))
     .subscribe({
       next: (data:any)=>{
-        // this.resultado=true;
         if(data.results.length>=2){
-        this.receta=data.results;
-        this.loading=false;
-        this.resultado=false;
-        console.log(data.results)
-        console.log(this.receta)
+       (data.results.forEach((element:any) => {this.id.push(element.id)}));
         }else if(data.results.length<=2){
           this.resultado=true;
         }
