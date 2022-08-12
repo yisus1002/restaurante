616e9cc121ab37d401136da14fa5bffa1f1fcffd
yisus1002 <yisusgarcia1002@gmail.com>
2022-08-11 21:44:18 -0500

    Se crearon los metodos para agregar, eliminar, obtener el menu  y las recetas por individuales

--- src/app/services/buscarreceta.service.ts
+++ src/app/services/buscarreceta.service.ts
@@ -1,32 +1,38 @@
 import { finalize, map } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BuscarrecetaService {
   receta:any[]=[];
-  menu:any[]=[1,2,3,4];
+  menu:any[]=[];
   loading:boolean=false;
   resultado:boolean=false;
   id:any[]=[];
   api:string='https://api.spoonacular.com/recipes/';
-  apykey:string='8a0b99c7da0c4d1dadfa51797ad3b73e';
+  apykey:string='2ffb93eb9d1f4806ab91ae151b9a644d';
 
   constructor(public http: HttpClient) {
-    // this.obtener()
+    this.obtenermenu();
    }
 
-  getRecetas(termino:string, ){
+  public getRecetas(termino:string, ){
     const url =`${this.api}complexSearch?query=${termino}&apiKey=${this.apykey}`;
     
-    return this.http.get(url).pipe(map((data:any)=> data))
+    return this.http.get(url).pipe(map((data:any)=> data));
   };
-  getInformationReceta(ids:any){
+  public getInformationReceta(ids:any){
     const url = `${this.api}informationBulk?ids=${ids}&apiKey=${this.apykey}`
-    return this.http.get(url).pipe(map((data:any)=> data))
-  }
+    return this.http.get(url).pipe(map((data:any)=> data));
+  };
+  public getPlatoInformacion(id:any){
+    const url = `${this.api}${id}/information?apiKey=${this.apykey}`;
+    return this.http.get(url).pipe(map((data:any)=> data));
+  };
+
   obtener(termino:string){
     this.loading=true;
     this.getRecetas(termino)
@@ -70,4 +76,41 @@ export class BuscarrecetaService {
       },
     })
   }
+
+  agregar(id:any){
+   const eleme= this.receta.filter(receta=>receta['id']===id) 
+    const men= this.menu?.filter(menu=>menu['id'] ===id) 
+    if(this.menu.length<4){
+    if(men[0]?.id===eleme[0].id){
+      console.log('El plato ya se encuentra en el menu')
+    }else{
+      this.menu.push(eleme[0])
+      localStorage.setItem('data', JSON.stringify(this.menu))
+      Swal.fire({
+        position: 'bottom-end',
+        icon: 'success',
+        title: `El plato ${eleme[0].title} se agregó correctamenta`,
+        showConfirmButton: false,
+        timer: 1500
+      })
+    }
+    }else{
+      console.log('El menu ya se encuentra lleno')
+    }
+  }
+  obtenermenu(){
+  const data=(localStorage.getItem('data'))
+    if(data){
+  this.menu=JSON.parse(data!)
+    console.log(this.menu)
+  }
+  }
+  eliminar(id:any){
+    console.log(id)
+    this.menu.splice(id,1)
+    localStorage.setItem('data', JSON.stringify(this.menu))
+    this.obtenermenu()
+    // localStorage.removeItem('data')
+  }
+
 }
