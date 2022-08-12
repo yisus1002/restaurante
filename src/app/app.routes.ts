b0982d079c3e75ee75f36c559d6df55bcf23aef6
yisus1002 <yisusgarcia1002@gmail.com>
2022-08-11 21:24:45 -0500

    Se creo la ruta para ver el detalle del plato

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router'; 
 import { HomeComponent } from './components/home/home.component';
 import { LoguinComponent } from './components/loguin/loguin.component';
+import { PlatoDetalleComponent } from './components/plato-detalle/plato-detalle.component';
 import { 
     AuthGuardService as AuthGuard
 } from './services/auth-guard.service';
@@ -9,8 +10,7 @@ import {
 export const ROUTES: Routes = [
     { path: 'home', component: HomeComponent , canActivate: [AuthGuard]},
     { path: 'login', component: LoguinComponent },
-    // { path: 'search', component: SearchComponent }, 
-    // { path: 'artist/:id', component: ArtistaComponent }, 
+    { path: 'plato-detalle/:id', component: PlatoDetalleComponent, canActivate: [AuthGuard] },
     { path: '', pathMatch :'full', redirectTo: 'home'}, 
     { path: '**', pathMatch :'full', redirectTo: 'home'}, 
 ];
