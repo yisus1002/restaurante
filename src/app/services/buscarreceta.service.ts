a5458786341b18af838ad47bda3875934e69ecee
yisus1002 <yisusgarcia1002@gmail.com>
2022-08-12 20:21:55 -0500

    Se valido que el usuario solo pueda ingresar 2 platos veganos y 2 no veganos

--- src/app/services/buscarreceta.service.ts
+++ src/app/services/buscarreceta.service.ts
@@ -12,11 +12,15 @@ export class BuscarrecetaService {
   loading:boolean=false;
   resultado:boolean=false;
   id:any[]=[];
+  vegano:any;
+  novegano:any
   api:string='https://api.spoonacular.com/recipes/';
-  apykey:string='2ffb93eb9d1f4806ab91ae151b9a644d';
+  apykey:string='8a0b99c7da0c4d1dadfa51797ad3b73e';
+
 
   constructor(public http: HttpClient) {
     this.obtenermenu();
+    this.verifivarplatos(true,false)
    }
 
   public getRecetas(termino:string, ){
@@ -35,6 +39,7 @@ export class BuscarrecetaService {
 
   obtener(termino:string){
     this.loading=true;
+    this.receta=[]
     this.getRecetas(termino)
     .pipe(finalize(()=>{
       if(this.id.length>0){
@@ -62,6 +67,7 @@ export class BuscarrecetaService {
     .subscribe({
       next: (data:any)=>{
         if(data.results.length>=2){
+          this.id=[];
        (data.results.forEach((element:any) => {this.id.push(element.id)}));
         }else if(data.results.length<=2){
           this.resultado=true;
@@ -78,38 +84,98 @@ export class BuscarrecetaService {
   }
 
   agregar(id:any){
+    
    const eleme= this.receta.filter(receta=>receta['id']===id) 
     const men= this.menu?.filter(menu=>menu['id'] ===id) 
     if(this.menu.length<4){
-    if(men[0]?.id===eleme[0].id){
-      console.log('El plato ya se encuentra en el menu')
+       if(men[0]?.id===eleme[0].id){
+        Swal.fire({
+          position: 'top-end',
+          icon: 'warning',
+          title: 'El plato ya se encuentra en el menú',
+          showConfirmButton: false,
+          timer: 1500
+        })
+       }else{ 
+         let Vegano = this.filtrarTipoPlato(true);
+         let NoVegano= this.filtrarTipoPlato(false);
+         if(eleme[0].vegan===true && Vegano===0){
+           Swal.fire({
+             position: 'top-end',
+             icon: 'error',
+             title: 'No puede agregar mas platos veganos',
+             showConfirmButton: false,
+             timer: 1500
+           })
+         }else if(eleme[0].vegan===true && (Vegano===1 || Vegano=== 2)){
+           this.guardarLocalStorage(eleme)
+           this.verifivarplatos(true,false)
+         }else if(eleme[0].vegan===false && NoVegano===0){
+           Swal.fire({
+             position: 'top-end',
+             icon: 'error',
+             title: 'No puede agregar mas platos no veganos',
+             showConfirmButton: false,
+             timer: 1500
+           }) 
+         }else if(eleme[0].vegan===false && (NoVegano===1 || NoVegano=== 2)){
+          this.guardarLocalStorage(eleme)
+          this.verifivarplatos(true,false)
+         }else{
+
+         }
+       }
     }else{
-      this.menu.push(eleme[0])
-      localStorage.setItem('data', JSON.stringify(this.menu))
       Swal.fire({
-        position: 'bottom-end',
-        icon: 'success',
-        title: `El plato ${eleme[0].title} se agregó correctamenta`,
+        position: 'top-end',
+        icon: 'error',
+        title: 'El menu ya  se encuentra lleno',
         showConfirmButton: false,
         timer: 1500
       })
     }
-    }else{
-      console.log('El menu ya se encuentra lleno')
+  }
+  guardarLocalStorage(eleme:any){ 
+    this.menu.push(eleme[0])
+    localStorage.setItem('data', JSON.stringify(this.menu))
+    Swal.fire({
+      position: 'bottom-end',
+      icon: 'success',
+      title: `El plato ${eleme[0].title} se agregó correctamenta`, 
+      showConfirmButton: false,
+      timer: 1500
+    }) 
+  }
+  filtrarTipoPlato(estato:boolean){
+    const data= this.menu.map(menu=> menu.vegan).filter(menu=> menu===estato)
+    if(data.length===0){
+      return 2
+    }else if(data.length===1){
+      return 1
+    }else {
+      return 0
     }
   }
-  obtenermenu(){
+  verifivarplatos(tru:boolean, fal:boolean){
+    this.vegano= this.filtrarTipoPlato(tru)
+    this.novegano= this.filtrarTipoPlato(fal)
+  }
+  obtenermenu(){ 
+    this.verifivarplatos(true,false)
   const data=(localStorage.getItem('data'))
     if(data){
   this.menu=JSON.parse(data!)
     console.log(this.menu)
   }
+  
+
   }
   eliminar(id:any){
     console.log(id)
     this.menu.splice(id,1)
     localStorage.setItem('data', JSON.stringify(this.menu))
     this.obtenermenu()
+    // this.verifivarplatos(true,false)
     // localStorage.removeItem('data')
   }
 
